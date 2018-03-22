c9290101971304750ee2e7ee755c606ac5cb74ff
UO251129 <UO251129@uniovi.es>
2018-03-22 17:18:13 +0100

    Añadido datos del creador en incidencia

--- src/main/java/asw/controllers/InciManagerController.java
+++ src/main/java/asw/controllers/InciManagerController.java
@@ -3,13 +3,14 @@ package asw.controllers;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import asw.database.entities.Agent;
 import asw.database.entities.Incidence;
 import asw.database.location.Location;
 import asw.database.status.Status;
@@ -17,30 +18,36 @@ import asw.services.IncidenceService;
 
 @Controller
 public class InciManagerController {
-	
+
 	@Autowired
 	private IncidenceService inciService;
-	
-	
-	@ResponseBody
-	@RequestMapping(value = "/incidence/add",  method = RequestMethod.POST)
-	private String POSTaddIncidence(@ModelAttribute Incidence inci, @ModelAttribute Location loc) {
-		inci.setLocalizacion(loc);
-		inci.setStatus(Status.ABIERTA);
-		inciService.addIncidence(inci);
-		return "Incidencia con id: " +inci.getId()+" creada";
-	}
 
 	@RequestMapping(value = "/incidence/add")
 	private String GETaddIncidence(HttpSession session, Model model) {
-		if(session.getAttribute("agent")==null) {
-			
+		if (session.getAttribute("agent") == null) {
+
 			return "redirect:/login";
 		}
-		
+
 		model.addAttribute("inci", new Incidence());
 		model.addAttribute("loc", new Location());
 		return "/incidence/add";
 	}
-	
+
+	@RequestMapping(value = "/incidence/add", method = RequestMethod.POST)
+	private String POSTaddIncidence(HttpSession session, Model model, @ModelAttribute Incidence inci,
+			@ModelAttribute Location loc) {
+		
+		Agent agent = (Agent) session.getAttribute("agent");
+		
+		inci.setLocalizacion(loc);
+		inci.setStatus(Status.ABIERTA);
+		inci.setUser(agent.getEmail());
+		inci.setPassword(agent.getPassword());
+		inciService.addIncidence(inci);
+
+		model.addAttribute("inciId", inci.getId());
+
+		return "/incidence/confirm";
+	}
 }
